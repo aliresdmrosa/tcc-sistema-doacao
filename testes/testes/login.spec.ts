8f25c7e92524102cb160dfb691e65eab877a5978
Alires Rosa <alires.cardoso@ufpr.br>
2026-07-02 19:38:13 -0300

    test: adicionado estrutura inicial dos testes E2E com Playwright

---
+++ testes/testes/login.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:4200/');
+  await page.getByRole('textbox', { name: 'E-mail' }).click();
+  await page.getByRole('textbox', { name: 'E-mail' }).fill('admin@sistemadoacao.com');
+  await page.getByRole('textbox', { name: 'E-mail' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Senha' }).fill('admin123');
+  await page.getByRole('button', { name: 'Entrar' }).click();
+});
